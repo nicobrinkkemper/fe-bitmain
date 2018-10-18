fcd0b29bc8cafed77967baf341cfae2bfb911e73
Nico Brinkkemper <nico@brinkkemper.nl>
2018-10-18 14:51:15 +0200

    Updates

---
+++ precache-manifest.7e38c151836b3cf39c17cb8cc1131255.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f710ebd3180951133a77",
+    "url": "/fe-bitmain/static/js/runtime~main.f710ebd3.js"
+  },
+  {
+    "revision": "58ff109462103880fdd0",
+    "url": "/fe-bitmain/static/js/main.58ff1094.chunk.js"
+  },
+  {
+    "revision": "b76e9af7cf62c97f0491",
+    "url": "/fe-bitmain/static/js/1.b76e9af7.chunk.js"
+  },
+  {
+    "revision": "58ff109462103880fdd0",
+    "url": "/fe-bitmain/static/css/main.1a3389c7.chunk.css"
+  },
+  {
+    "revision": "423f878e390ecacc94a519728137b628",
+    "url": "/fe-bitmain/index.html"
+  }
+];
